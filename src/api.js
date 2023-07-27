ecf68cc8511c12151e21f8c0ef8988d9a5f426ac
denisfl1 <denisfl1@hotmail.com>
2023-07-27 18:21:26 -0300

    apiupdate

--- src/api.js
+++ src/api.js
@@ -18,11 +18,11 @@ const logout = ()=>{
 
 
 
-export const SOCKET= io('http://localhost:3000/',{maxBufferSize:50,extraHeaders:{Authorization:localStorage.getItem("token")}})
+export const SOCKET= io(process.env.REACT_APP_URL,{maxBufferSize:50,extraHeaders:{Authorization:localStorage.getItem("token")}})
 
 
 export const api = axios.create({
-        baseURL: 'http://localhost:3000/'
+        baseURL: process.env.REACT_APP_URL
     })
 
     
